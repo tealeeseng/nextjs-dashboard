60358dd246e9a6832c8b9dadf4563bff23b64b8d
Lee Seng <tealeeseng@gmail.com>
2024-08-06 11:08:26 +0800

    fetching data tutorial

--- scripts/seed.js
+++ scripts/seed.js
@@ -161,6 +161,12 @@ async function seedRevenue(client) {
 }
 
 async function main() {
+  console.log({
+    POSTGRES_URL: process.env.POSTGRES_URL,
+    POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING
+  });
+
+
   const client = await db.connect();
 
   await seedUsers(client);
