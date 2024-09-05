af443c69e7040a61763f1c84a82d0dc75643c8e8
lee seng <tealeeseng@gmail.com>
2024-09-05 14:38:09 +0800

    Chapter 15 auth

--- googe-vertex.py
+++
@@ -1,9 +0,0 @@
-import base64
-import json
-
-with open('path/to/your/service-account-file.json', 'r') as file:
-    service_account_info = file.read()
-
-encoded_key = base64.b64encode(
-    service_account_info.encode('utf-8')).decode('utf-8')
-print(encoded_key)
